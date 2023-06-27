6ac69f562acf01d6d6dd41ad3e3b00177cc98f07
GaneshDevM <100254102+GaneshDevM@users.noreply.github.com>
2023-06-27 11:00:30 +0530

    check

--- app.js
+++ app.js
@@ -10,3 +10,7 @@ app.get("/", (req, res)=>{
 app.listen(3000,function (){
     console.log("listening at 3000")
 })
+
+app.get("/about", (req, res)=>{
+   res.send(<h1>created by Ganesh Mundkar</h1>)
+})
